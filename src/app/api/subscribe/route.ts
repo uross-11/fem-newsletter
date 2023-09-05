cab63704f9fcd8fb156ab3d7c4517ca6a22bad6c
uros <u.ignjatovic00@gmail.com>
2023-09-05 17:47:53 +0200

    feat: mailchimp journey api

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -12,13 +12,15 @@ export async function POST(req: Request) {
     const LIST_ID = process.env.MAILCHIMP_LIST_ID;
     const API_KEY = process.env.MAILCHIMP_API_KEY;
     const DATACENTER = API_KEY!.split("-")[1];
+    const JOURNEY_ID = process.env.MAILCHIMP_JOURNEY_ID;
+    const STEP_ID = process.env.MAILCHIMP_STEP_ID;
 
     const data = {
       email_address: email,
       status: "subscribed",
     };
 
-    const response = await fetch(
+    const subscribe = await fetch(
       `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`,
       {
         body: JSON.stringify(data),
@@ -30,8 +32,31 @@ export async function POST(req: Request) {
       },
     );
 
-    if (response.status >= 400) {
-      const { detail } = await response.json();
+    if (subscribe.status >= 400) {
+      const { detail } = await subscribe.json();
+
+      return NextResponse.json(
+        {
+          error: detail || "There was an error subscribing to the newsletter.",
+        },
+        { status: 400 },
+      );
+    }
+
+    const journey = await fetch(
+      `https://${DATACENTER}.api.mailchimp.com/3.0/customer-journeys/journeys/${JOURNEY_ID}/steps/${STEP_ID}/actions/trigger`,
+      {
+        body: JSON.stringify(data),
+        headers: {
+          Authorization: `apiKey ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      },
+    );
+
+    if (journey.status >= 400) {
+      const { detail } = await journey.json();
 
       return NextResponse.json(
         {
